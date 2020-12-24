613d0f35f457fdfa102f717f3ac6758628e662e2
victoriengb <victoriengbaguidi@gmail.com>
2020-12-24 08:12:22 +0100

    Modification classe LotStock

--- Biilomo/src/entrepot_lot/LotStock.java
+++ Biilomo/src/entrepot_lot/LotStock.java
@@ -18,6 +18,20 @@ public class LotStock extends LotType{
 	}
 
 	/**
+	 * Permet d'ajouter un certain volume de pièces au stock
+	 * @param volume
+	 */
+	
+	public void incrementerVolumeStock(int volume) {
+		
+		/*
+		 * FAIRE UNE EXCEPTION POUR S'ASSURER QUE LE PARAMETRE EST STRICTEMENT POSITIF PUIS
+		 * SUPPRIMER LE COMMENTAIRE
+		 */
+		this.volume+= volume;
+	}
+	
+	/**
 	 * Retourne une description textuelle des objets LotStock
 	 * @see java.lang.Object#toString()
 	 * 
