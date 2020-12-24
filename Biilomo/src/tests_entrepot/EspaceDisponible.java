c8f47ccd6de1e1b0b151bb3a01bcc714b3a70ab5
victoriengb <victoriengbaguidi@gmail.com>
2020-12-24 08:07:43 +0100

    Ajout Classe Test EspaceDisponible

---
+++ Biilomo/src/tests_entrepot/EspaceDisponible.java
@@ -0,0 +1,14 @@
+package tests_entrepot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EspaceDisponible {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
