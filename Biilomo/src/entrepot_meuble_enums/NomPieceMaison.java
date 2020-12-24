01b764b1138a7cf919461a29407e3cd0889b09d5
hafbn <gbn131100@gmail.com>
2020-12-24 17:05:44 +0100

    L'enumeration nomPieceMaison.java - non fonctionnel

--- Biilomo/src/entrepot_meuble_enums/NomPieceMaison.java
+++ Biilomo/src/entrepot_meuble_enums/NomPieceMaison.java
@@ -1,8 +1,32 @@
 package entrepot_meuble_enums;
 
 public enum NomPieceMaison {
-
-	SALON,
-	CUISINE,
-	PIECEMAISONQCQ;
+	salon, chambre_a_coucher, salle_a_manger, cuisine, salle_de_bains, hall, buanderie, grenier, sous_sol, balcon;
+	
+	public void getNomPieceMaison() {
+		switch(this) {
+		case salon:
+			System.out.println("salon");
+		case chambre_a_coucher:
+			System.out.println("chambre_a_coucher");
+		case salle_a_manger:
+			System.out.println("salle_a_manger");
+		case cuisine:
+			System.out.println("cuisine");
+		case salle_de_bains:
+			System.out.println("salle_de_bains");
+		case hall:
+			System.out.println("hall");
+		case buanderie:
+			System.out.println("buanderie");
+		case grenier:
+			System.out.println("grenier");
+		case sous_sol:
+			System.out.println("sous_sol");
+		case balcon:
+			System.out.println("balcon");
+		defaut:
+			System.out.println("null");
+		}
+	}
 }
