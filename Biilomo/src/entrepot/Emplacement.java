5aa03dfc780cd1fb7837b641e51abaf2e75b15b2
victoriengb <victoriengbaguidi@gmail.com>
2020-12-24 08:29:05 +0100

    Classe Emplacement - Nouvelle Organisation

--- Biilomo/src/entrepot/Emplacement.java
+++ Biilomo/src/entrepot/Emplacement.java
@@ -8,20 +8,50 @@ package entrepot;
  */
 public class Emplacement {
 
-	private EspaceDisponible espace;
+	/**
+	 * Indique la disponibilité d'un emplacement
+	 */
+	private boolean isDisponible;
 	
 	/**
 	 * Constructeur par défaut de la classe Emplacement
 	 */
 	public Emplacement() {
-		espace= new EspaceDisponible();
+		this.isDisponible= true;
 	}
 	
 	/**
-	 * Permet de récupérer l'attribut espace
-	 * @return {@link EspaceDisponible}
+	 * Retourne une description textuelle de l'objet Emplacement
+	 * @see java.lang.Object#toString()
 	 */
-	public EspaceDisponible getEspace() {
-		return this.espace;
+	
+	/**
+	 * Retourne l'attribut isDiponible
+	 * @return boolean
+	 */
+	public boolean getIsDisponible() {
+		return this.isDisponible;
+	}
+	
+	/**
+	 * Affecte la valeur true à l'attribut isDisponible
+	 */
+	public void setDisponible() {
+		this.isDisponible= true;
+	}
+	
+	/**
+	 * Affecte la valeur false à l'attribut isDisponible
+	 */
+	public void setIndisponible() {
+		this.isDisponible= false;
+	}
+	
+	@Override
+	public String toString() {
+		if(this.isDisponible == true) {
+			return "Disponible";
+		}
+		return "Indisponible";
 	}
 }
