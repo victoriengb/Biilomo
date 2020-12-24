c2feb053e3a675ad7e61933c1a26a4a49bf04acf
victoriengb <victoriengbaguidi@gmail.com>
2020-12-24 08:12:01 +0100

    Modification Classe Lot

--- Biilomo/src/entrepot_lot/Lot.java
+++ Biilomo/src/entrepot_lot/Lot.java
@@ -16,7 +16,17 @@ public class Lot extends LotType {
 	/**
 	 * Id d'un lot
 	 */
-	private int id;
+	private Integer id;
+	
+	/**
+	 * Indique l'espace occupé par le lot à droite et à gauche de sa case initiale
+	 */
+	private EspaceOccuppeLot espaceOccuppe;
+	
+	/**
+	 * Indique la position du lot dans l'inventaire
+	 */
+	private PositionLot positionLot;
 	
 	/**
 	 * Contructeur paramétré de la classe Lot
@@ -26,10 +36,35 @@ public class Lot extends LotType {
 	public Lot(int volume, Piece piece) {
 		
 		super(volume, piece);
+		this.espaceOccuppe= new EspaceOccuppeLot();
+		this.positionLot= new PositionLot();
 		this.id= countId;
 		countId++;
 	}
 	
+	/**
+	 * Renvoie l'attribut espaceOccuppe
+	 * @return {@link EspaceOccuppeLot}
+	 */
+	public EspaceOccuppeLot getEspaceOccuppe() {
+		return this.espaceOccuppe;
+	}
+	
+	/**
+	 * Renvoie l'attribut positionLot
+	 * @return {@link PositionLot}
+	 */
+	public PositionLot getPositionLot() {
+		return this.positionLot;
+	}
+	
+	/**
+	 * Affecte une nouvelle position au lot
+	 * @param position
+	 */
+	public void setPositionLot(PositionLot position) {
+		this.positionLot= position;
+	}
 	/**
 	 * Retourne une description textuelle des objets Lot
 	 * @see java.lang.Object#toString()
